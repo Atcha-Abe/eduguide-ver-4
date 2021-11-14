88d0ee249e60d1a7915f87b9da6fc00e40acfab4
MCBlankCams <camillemahilum@gmail.com>
2021-11-15 04:09:09 +0800

    Gen Test Sample
    wala pang set of questions kasi sineset pa yung question na ilalagay

--- src/generaltest/GenTest.js
+++ src/generaltest/GenTest.js
@@ -1,44 +1,147 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./General.css";
 
-import { Link } from "react-router-dom";
-export default class GenTest extends Component {
-  render() {
-    return (
-      <div align="center">
-        <h3>Career Decision Support General Test</h3>
-        <br></br>
-        <p className="ques-count">Question 1 out of 10</p>
-        <br></br>
-        <div className="question">
-          <label className="gen-label">
-            How interested are you in Computers?
-          </label>
-          <div align="center">
-            <input type="radio" className="gen-input" />
-            <label className="gen-label">1</label>
-            <input type="radio" className="gen-input" />
-            <label className="gen-label">2</label>
-            <input type="radio" className="gen-input" />
-            <label className="gen-label">3</label>
-            <input type="radio" className="gen-input" />
-            <label className="gen-label">4</label>
-            <input type="radio" className="gen-input" />
-            <label className="gen-label">5</label>
-          </div>
-        </div>
-        <div className="result-btn">
-          <Link to="/gendesc" className="reg-btn" value="Back">
-            Back
-          </Link>
-          <Link to="/genresults" className="reg-btn" value="Next">
-            Next
-          </Link>
-        </div>
-        <p className="bottom_p">
-          <b>Make the right decision.</b>
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+	const questions = [
+		{
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in computers?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in teaching?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in health care practices?',
+			answerOptions: [
+        { answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in wound care?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in emergency care?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in analyzing business environment for strategic direction?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in conduction business research?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in endorsing products, goods and services?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in the work flow of products, goods and services?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'On a scale of 1-5 where 5 is the highest, how interested are you in starting up a business?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '2', isCorrect: false },
+				{ answerText: '3', isCorrect: true },
+				{ answerText: '4', isCorrect: false },
+        { answerText: '5', isCorrect: false },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app' align='center'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
