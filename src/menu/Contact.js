9e40ad9aaa6e5d21603c422b5e7f7147ea91bc86
Migs Carabeo <migscarabeo@gmail.com>
2021-11-28 21:27:07 +0800

    Footer, Blog and Contact

--- src/menu/Contact.js
+++ src/menu/Contact.js
@@ -1,22 +1,45 @@
-import React, { Component } from "react";
-export default class Contact extends Component {
-  render() {
-    return (
+import React, { useRef } from "react";
+import emailjs from 'emailjs-com'
+
+
+export default function Contact() {
+  const form = useRef();
+ let sendEmail = (e) => {
+    e.preventDefault();
+  
+    emailjs.sendForm('gmail', 'template_4r7zsnq', form.current, 'user_k2uyNdRwq3XhF0ItCc5pZ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
+
+  return(
+     
       <div>
         <h1>Connect with us</h1>
         <div align="center">
+          <a href="https://www.messenger.com/t/110839054741107">
           <img src="./icons/Messenger.png" className="social-icons"></img>
+          </a>
+
+          <a href="mailto:eduguide.acmt@gmail.com/?subject=Questions and Concerns">
           <img src="./icons/GMail.png" className="social-icons"></img>
+          </a>
         </div>
 
         <div>
-          <form align="center">
+          <form align="center" ref={form} onSubmit={sendEmail}>
             <label>
               <br></br>
               Send your feedback here
               <br></br>
               <br></br>
-              <input type="text" className="contact-field" name="name" />
+              <input type="text" className="contact-field" name="message" />
+              {/* <input type="email" className="contact-field" name="email_here" /> */}
             </label>
             <br></br>
             <br></br>
@@ -24,6 +47,5 @@ export default class Contact extends Component {
           </form>
         </div>
       </div>
-    );
-  }
+  );
 }
