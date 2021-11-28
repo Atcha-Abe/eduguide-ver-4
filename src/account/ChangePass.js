d39045a5d68405de75a8cd1d9d4870588aef41f8
Atcha Abe <athenaaliafeabe@gmail.com>
2021-11-29 05:17:54 +0800

    Revert "change password"
    This reverts commit 734991eae3c1a62c7d06fbc2921395d7115ac3c2.

--- src/account/ChangePass.js
+++ src/account/ChangePass.js
@@ -2,74 +2,18 @@ import React, { useState } from "react";
 import "./Account.css";
 import { UPDATE_PASSWORD } from "../account/Graphql/Mutation";
 import { useMutation } from "@apollo/client";
-import axios from "axios";
 
 import { Link } from "react-router-dom";
-
 function ChangePass() {
-  const [values, setValues] = useState(
-    {
-      userName: '',
-      currentPassword: '',
-      newPassword: '',
-    }
-  );
-  const [errors, setErrors] = useState({});
+  const [username, setUsername] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   const [updatePassword, { error }] = useMutation(UPDATE_PASSWORD);
-
-  const handleInputChange = e => {
-    const { id, value } = e.target
-    setValues({ ...values, [id]: value })
-  }
-
-  const changePassword = () => {    
-    let errors = {};
-    if (values.userName.trim() === '') {
-      errors.userName = 'Enter user name.';
-    }
-
-    if (values.currentPassword === '') {
-      errors.currentPassword = 'Enter Current Password.';
-    } 
-    
-    if (values.newPassword === '') {
-      errors.newPassword = 'Enter New Password.';
-    } 
-
-    if (errors.userName ||
-      errors.currentPassword ||
-      errors.newPassword
-    ) {
-      setErrors(errors);
-      return;
-    }
-
-    updatePassword({
-      variables: {
-        username: values.userName,
-        oldPassword: values.currentPassword,
-        newPassword: values.newPassword,  
-      },
-    });
-
-    axios.post(baseUrl + 'api/changePassword', {
-      username: values.userName,
-      oldPassword: values.currentPassword,
-      newPassword: values.newPassword,      
-    }) 
-    .then((response) => {
-      alert(response.data.result);      
-    })
-    .catch((error) => {
-      alert(error);
-    });
-  };
 
   if (error) {
     return <h1> {error} </h1>;
   }
-
   return (
     <div align="center">
       <h3>Change Password</h3>
@@ -77,34 +21,37 @@ function ChangePass() {
         <label>
           <b>Username:</b>
           <input
-            type="userName"
+            type="username"
             className="pass-field"
-            name="userName"
-            onChange={handleInputChange}
+            name="username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
           />
-          {errors.userName && <p className="form-error">{errors.userName}</p>}
         </label>
         <br></br>
         <label>
           <b>Password:</b>
           <input
-            type="currentPassword"
+            type="password"
             className="pass-field"
-            name="currentPassword"
-            onChange={handleInputChange}
+            name="password"
+            onChange={(event) => {
+              setCurrentPassword(event.target.value);
+            }}
           />
-          {errors.currentPassword && <p className="form-error">{errors.currentPassword}</p>}
         </label>
         <br></br>
         <label>
           <b>New Password:</b>
           <input
-            type="newPassword"
+            type="password"
             className="pass-field"
-            name="newPassword"
-            onChange={handleInputChange}
+            name="npassword"
+            onChange={(event) => {
+              setNewPassword(event.target.value);
+            }}
           />
-          {errors.newPassword && <p className="form-error">{errors.newPassword}</p>}
         </label>
       </form>
       <div className="pass-btns">
@@ -112,7 +59,15 @@ function ChangePass() {
           to="/save"
           className="reg-btn"
           value="Save"
-          onClick={changePassword}
+          onClick={() => {
+            updatePassword({
+              variables: {
+                username: username,
+                oldPassword: currentPassword,
+                newPassword: newPassword,
+              },
+            });
+          }}
         >
           Save
         </Link>
